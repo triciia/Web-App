195eaa70709efae30dd46f3c6436a0afcc4cc08c
Patricia <30eds.pat@gmail.com>
2025-08-05 17:53:46 -0700

    added cards
    logo + name of stores populated

--- script.js
+++ script.js
@@ -32,7 +32,14 @@ async function getAllRecords() {
         
 
         newHtml += `
- 
+         <div class="card" style="width: 18rem;">
+  <img src="${logo[0].url}" class="card-img-top" alt="Store Logo">
+  <div class="card-body">
+    <h5 class="card-title">${name}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> 
     
         
         `;
